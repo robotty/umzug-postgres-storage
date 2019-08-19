bb936915c42049aa49b0819a3f110ba92c97a562
Ruben Anders <ruben.anders@robotty.de>
2019-08-19 23:26:57 +0200

    Reformat with prettier + add more linting

--- README.md
+++ README.md
@@ -7,48 +7,51 @@ Also featuring a small utility function for running migrations atomically using 
     npm i @robotty/umzug-postgres-storage
 
 ```javascript
-import { Pool } from 'pg';
-import { PGStorage, migrateInTransaction } from '@robotty/umzug-postgres-storage';
-import * as Umzug from 'umzug';
+import { Pool } from "pg";
+import {
+  PGStorage,
+  migrateInTransaction
+} from "@robotty/umzug-postgres-storage";
+import * as Umzug from "umzug";
 // const { Pool } = require('pg');
 // const { PGStorage, migrateInTransaction } = require('@robotty/umzug-postgres-storage');
 // const Umzug = require('umzug');
 
 async function runMigrations() {
-    // for options, see: https://node-postgres.com/features/connecting
-    let dbPool = new Pool({
-        database: 'testdb',
-        host: '/var/run/postgresql',
+  // for options, see: https://node-postgres.com/features/connecting
+  let dbPool = new Pool({
+    database: "testdb",
+    host: "/var/run/postgresql"
+  });
+
+  // migrateInTransaction begins and commits/rollbacks a transaction for you
+  // if undesired, omit this wrapper and use:
+  // let db = await dbPool.connect();
+
+  // whatever you return from the async function inside gets returned to here
+  // (note that migrateInTransaction also accepts single connections, not only pools)
+  let migrations = await migrateInTransaction(dbPool, async db => {
+    let umzug = new Umzug({
+      // second parameter (config) is entirely optional
+      storage: new PGStorage(db, {
+        tableName: "MyAppMigration", // optional (default is SchemaMigration)
+        columnName: "MyAppRevisionID" // optional (default is RevisionID)
+      }),
+      migrations: {
+        // passes the db connection to use to the first parameter of all the up()/down() migrations
+        // you defined for umzug to run
+        params: [db]
+      }
     });
 
-    // migrateInTransaction begins and commits/rollbacks a transaction for you
-    // if undesired, omit this wrapper and use:
-    // let db = await dbPool.connect();
+    return await umzug.up();
+  });
 
-    // whatever you return from the async function inside gets returned to here
-    // (note that migrateInTransaction also accepts single connections, not only pools)
-    let migrations = await migrateInTransaction(dbPool, async db => {
-        let umzug = new Umzug({
-            // second parameter (config) is entirely optional
-            storage: new PGStorage(db, {
-                tableName: "MyAppMigration", // optional (default is SchemaMigration)
-                columnName: "MyAppRevisionID" // optional (default is RevisionID)
-            }),
-            migrations: {
-                // passes the db connection to use to the first parameter of all the up()/down() migrations
-                // you defined for umzug to run
-                params: [db]
-            }
-        });
-        
-        return await umzug.up();
-    });
-    
-    console.log("Successfully ran these migrations:", migrations)
+  console.log("Successfully ran these migrations:", migrations);
 }
 
 runMigrations().catch(e => {
-    console.error(e);
-    process.exit(-1);
+  console.error(e);
+  process.exit(-1);
 });
 ```
--- lib/index.ts
+++ lib/index.ts
@@ -1,103 +1,103 @@
-import { ClientBase, Pool, PoolClient, QueryArrayResult } from 'pg';
-import { Storage } from 'umzug';
-import { ident } from 'pg-escape';
+import { ClientBase, Pool, PoolClient, QueryArrayResult } from "pg";
+import { ident } from "pg-escape";
+import { Storage } from "umzug";
 
 export interface PGStorageConfiguration {
-    tableName: string;
-    columnName: string;
+  tableName: string;
+  columnName: string;
 }
 
 const configDefaults: PGStorageConfiguration = {
-    tableName: 'SchemaMigration',
-    columnName: 'RevisionID'
+  tableName: "SchemaMigration",
+  columnName: "RevisionID"
 };
 
 export class PGStorage implements Storage {
-    private readonly db: ClientBase;
-    private readonly config: PGStorageConfiguration;
-    private tableCreated = false;
+  private readonly db: ClientBase;
+  private readonly config: PGStorageConfiguration;
+  private tableCreated = false;
 
-    public constructor(db: ClientBase, partialConfig: Partial<PGStorageConfiguration> = {}) {
-        this.db = db;
-        this.config = Object.assign({}, partialConfig, configDefaults);
+  public constructor(
+    db: ClientBase,
+    partialConfig: Partial<PGStorageConfiguration> = {}
+  ) {
+    this.db = db;
+    this.config = { ...configDefaults, ...partialConfig };
+  }
+
+  public async executed(): Promise<string[]> {
+    await this.createTable();
+    const result: QueryArrayResult = await this.db.query({
+      text: `
+        SELECT ${ident(this.config.columnName)}
+        FROM ${ident(this.config.tableName)}
+        ORDER BY ${ident(this.config.columnName)} ASC`,
+      rowMode: "array"
+    });
+
+    return result.rows.flat();
+  }
+
+  public async logMigration(migrationName: string): Promise<void> {
+    await this.createTable();
+    await this.db.query({
+      text: `
+        INSERT INTO ${ident(this.config.tableName)}
+          (${ident(this.config.columnName)})
+          VALUES ($1)
+          ON CONFLICT DO NOTHING`,
+      values: [migrationName]
+    });
+  }
+
+  public async unlogMigration(migrationName: string): Promise<void> {
+    await this.createTable();
+    await this.db.query({
+      text: `
+        DELETE FROM ${ident(this.config.tableName)}
+        WHERE ${ident(this.config.columnName)} = $1`,
+      values: [migrationName]
+    });
+  }
+
+  private async createTable(): Promise<void> {
+    if (this.tableCreated) {
+      return;
     }
 
-    private async createTable(): Promise<void> {
-        if (this.tableCreated) {
-            return;
-        }
-
-        await this.db.query(`
-            CREATE TABLE IF NOT EXISTS ${ident(this.config.tableName)} (
-                ${ident(this.config.columnName)} TEXT NOT NULL PRIMARY KEY UNIQUE
-            )
-        `);
-        this.tableCreated = true;
-    }
-
-    public async executed(): Promise<string[]> {
-        await this.createTable();
-        let result: QueryArrayResult = await this.db.query({
-            text: `
-                    SELECT ${ident(this.config.columnName)}
-                    FROM ${ident(this.config.tableName)}
-                    ORDER BY ${ident(this.config.columnName)} ASC
-                `,
-            rowMode: 'array'
-        });
-
-        return result.rows.flat();
-    }
-
-    public async logMigration(migrationName: string): Promise<void> {
-        await this.createTable();
-        await this.db.query({
-            text: `
-                INSERT INTO ${ident(this.config.tableName)}
-                    (${ident(this.config.columnName)})
-                    VALUES ($1)
-                    ON CONFLICT DO NOTHING
-            `,
-            values: [migrationName]
-        });
-    }
-
-    public async unlogMigration(migrationName: string): Promise<void> {
-        await this.createTable();
-        await this.db.query({
-            text: `
-                DELETE FROM ${ident(this.config.tableName)}
-                    WHERE ${ident(this.config.columnName)} = $1
-            `,
-            values: [migrationName]
-        });
-    }
+    await this.db.query(`
+      CREATE TABLE IF NOT EXISTS ${ident(this.config.tableName)} (
+        ${ident(this.config.columnName)} TEXT NOT NULL PRIMARY KEY UNIQUE
+      )`);
+    this.tableCreated = true;
+  }
 }
 
-export async function migrateInTransaction<T>(db: Pool | ClientBase, cb: (db: ClientBase) => Promise<T>): Promise<T> {
-    let conn: ClientBase | PoolClient;
-    let poolClient: boolean;
+export async function migrateInTransaction<T>(
+  db: Pool | ClientBase,
+  cb: (db: ClientBase) => Promise<T>
+): Promise<T> {
+  let conn: ClientBase | PoolClient;
+  if (db instanceof Pool) {
+    conn = await db.connect();
+  } else {
+    conn = db;
+  }
+
+  try {
+    await conn.query("BEGIN TRANSACTION");
+
+    const result = await cb(conn);
+    await conn.query("COMMIT");
+    return result;
+  } catch (e) {
+    await conn.query("ROLLBACK");
+    throw e;
+  } finally {
     if (db instanceof Pool) {
-        conn = await db.connect();
-        poolClient = true;
-    } else {
-        conn = db;
-        poolClient = false;
+      // eslint-disable-next-line @typescript-eslint/ban-ts-ignore
+      // @ts-ignore release() does not exist on BaseClient
+      conn.release();
     }
-
-    try {
-        await conn.query('BEGIN TRANSACTION');
-
-        let result = await cb(conn);
-        await conn.query('COMMIT');
-        return result;
-    } catch (e) {
-        await conn.query('ROLLBACK');
-        throw e;
-    } finally {
-        if (poolClient) {
-            // @ts-ignore
-            conn.release();
-        }
-    }
+  }
 }
